fca566ccd3b0dc52d9af24d2810bf4d7904e5c9e
AliG2 <73692494+AliG2@users.noreply.github.com>
2022-02-10 10:46:41 +0100

    Spring Boot init

---
+++ src/test/java/ali/roozbeh/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.roozbeh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
