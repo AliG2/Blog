398db7a1be3d8dc1267b782483bef8f794ea9a23
Roozbeh <mmroozbeh2000@gmail.com>
2022-02-10 11:29:00 +0100

    Blogapp test

--- src/main/java/ali/roozbeh/blog/BlogApplication.java
+++ src/main/java/ali/roozbeh/blog/BlogApplication.java
@@ -1,5 +1,7 @@
 package ali.roozbeh.blog;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+        System.out.println("roozbeh");
+
     }
 
 }
