27e4488f8cb3259cd2ef17987e501071da11b7b0
AliG2 <73692494+AliG2@users.noreply.github.com>
2022-03-23 23:33:48 +0100

    parkoding, Vi har gjort; post, category, categoryRepository,postrepository och postsservice!

---
+++ src/main/java/modules/users/modules/CategoryRepository.java
@@ -0,0 +1,10 @@
+package modules.users.modules;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/modules/users/modules/CategoryService.java
@@ -0,0 +1,37 @@
+package modules.users.modules;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Transactional
+    public Category registerCategory(Category category) {
+        return this.categoryRepository.save(category);
+    }
+
+    public List<Category> findAllCategories() {
+        return this.categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        return categoryRepository.getOne(id);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
