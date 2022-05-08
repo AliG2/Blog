111cbeb195d9828efd9978baf1e18915cbc4b53a
Roozbeh <mmroozbeh2000@gmail.com>
2022-05-08 21:52:57 +0200

    FIXING BUGS och snygggöring ! PARKODING

--- src/main/java/ali/roozbeh/blog/modules/posts/controller/CategoryController.java
+++ src/main/java/ali/roozbeh/blog/modules/posts/controller/CategoryController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryController {
 
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @Autowired
     public CategoryController(CategoryService categoryService) {
--- src/main/java/ali/roozbeh/blog/modules/posts/repository/PostsRepository.java
+++ src/main/java/ali/roozbeh/blog/modules/posts/repository/PostsRepository.java
@@ -17,7 +17,7 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     //    @Query("select p from Posts p where :#{#posts.title} is null or p.title like concat('%',:#{#posts.title},'%')")
     @Query("select p from Posts p join p.categories pc where (:#{#posts.title} is null or " +
             "p.title like concat('%',:#{#posts.title},'%')) and " +
-            "(coalesce(:#{#posts.categories},null) is null or " +
+            "(coalesce(:#{#posts.categories},null ) is null or " +
             "pc in (:#{#posts.categories})) " +
             "group by p.id having count (p.id) >= :num")
     Page<Posts> findBySearch(Posts posts, @Param("num") Long size, Pageable pageable);
